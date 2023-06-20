57ad042b7844917ac1b8084d8c177eb33a7ce7e1
Julio Ferreyra <jaft210403@outlook.com>
2023-06-20 17:44:00 -0600

    probando github

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ButtonsScreen extends StatelessWidget {
-
- static const String name = "buttons_screen";
+  static const String name = "buttons_screen";
 
   const ButtonsScreen({super.key});
 
@@ -10,7 +10,36 @@ class ButtonsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Buttons Screen")),
-      body:  const Placeholder(),
+      body:const _ButtonsView(),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.arrow_back_ios_new),
+        onPressed: () => context.pop(),
+      ),
+    );
+  }
+}
+
+class _ButtonsView extends StatelessWidget {
+  const _ButtonsView();
+
+  @override
+  Widget build(BuildContext context) {
+    return  SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric( horizontal: 10, vertical: 20),
+    
+        child: Wrap(
+          crossAxisAlignment: WrapCrossAlignment.center,
+          alignment: WrapAlignment.center,
+          spacing: 10,
+          children: [
+            ElevatedButton(onPressed: () {}, child: const Text("Elevated")),
+            const ElevatedButton(onPressed: null, child: Text("Elevated Diabled")),
+            ElevatedButton.icon(onPressed: () {} , icon: const Icon(Icons.access_alarm_outlined),label: const Text("Icon"),),
+          ],
+        ),
+      ),
     );
   }
 }
