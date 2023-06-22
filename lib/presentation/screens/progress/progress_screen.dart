84352c9e3cfa550ea7b8dadf205b1a81d8f75fa4
Julio Ferreyra <jaft210403@outlook.com>
2023-06-21 19:06:11 -0600

    Controlled Progress indicator agregado a la aplicacion

--- lib/presentation/screens/progress/progress_screen.dart
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -28,14 +28,48 @@ class _ProgressView extends StatelessWidget {
             height: 30,
           ),
           Text("Circular Progress Indicator"),
-          SizedBox(
-            height: 30,
-          ),
+          SizedBox(height: 30),
           CircularProgressIndicator(
-            strokeWidth: 2, backgroundColor: Colors.transparent,
-          )
+              strokeWidth: 2, backgroundColor: Colors.transparent),
+          SizedBox(height: 30),
+          Text("Circular Progress Indicator controlado"),
+          SizedBox(height: 10),
+          _ControlledProgressIndicator()
         ],
       ),
     );
+  }
+}
+
+class _ControlledProgressIndicator extends StatelessWidget {
+  const _ControlledProgressIndicator();
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+        stream: Stream.periodic(const Duration(milliseconds: 300), (value) =>
+         (value * 2) / 100
+        ).takeWhile((value) => value >= 0),
+        builder: (context, snapshot) {
+          final double progressValue = snapshot.data ?? 0;
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircularProgressIndicator(
+                  value: progressValue,
+                  strokeWidth: 2,
+                  backgroundColor: Colors.transparent,
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                    child: LinearProgressIndicator(
+                  value: progressValue,
+                ))
+              ],
+            ),
+          );
+        });
   }
 }
