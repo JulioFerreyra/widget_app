371a31ac1edde35cd22682d643f619c774857145
Julio Ferreyra <jaft210403@outlook.com>
2023-06-22 09:36:27 -0600

    snacbarsk terminados

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SnackbarScreen extends StatelessWidget {
   const SnackbarScreen({super.key});
@@ -9,6 +10,28 @@ class SnackbarScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Snackbars y dialogos"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            FilledButton(
+                onPressed: () {
+                  showAboutDialog(
+                    context: context,
+                    children: [
+                      const Text(
+                          "Eiusmod tempor nulla cupidatat do in cupidatat amet dolore deserunt labore ea elit do sunt. Id quis cillum aliqua et nostrud non sit in nulla quis ut. Voluptate nulla excepteur aute ullamco. Sit incididunt sint occaecat ipsum. Esse consectetur incididunt magna officia veniam consectetur nostrud nulla non irure nulla aute id.")
+                    ],
+                  );
+                },
+                child: const Text("Licencias Usadas")),
+            FilledButton(
+                onPressed: () {
+                  _openDialog(context);
+                },
+                child: const Text("Mostrar Dialogo")),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
@@ -23,7 +46,6 @@ class SnackbarScreen extends StatelessWidget {
 
   void _showCustomSnackbar(BuildContext context) {
     SnackBar snackbar = SnackBar(
-      
       content: const Text("Hola mundo"),
       action: SnackBarAction(
         label: "Hide",
@@ -32,5 +54,31 @@ class SnackbarScreen extends StatelessWidget {
       duration: const Duration(seconds: 2),
     );
     ScaffoldMessenger.of(context).showSnackBar(snackbar);
+  }
+
+  void _openDialog(BuildContext context) {
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text(
+          "¿Estás seguro?",
+        ),
+        content: const Text(
+            "Labore ex proident laboris cupidatat reprehenderit laboris sit in ex eu est exercitation irure. Eiusmod ipsum enim mollit mollit aliquip proident ex id in esse. Incididunt ex ipsum veniam esse officia elit do Lorem fugiat consequat. Ex culpa anim non in quis id eiusmod ipsum consectetur ad deserunt duis. Officia sint est labore anim ea elit magna officia. Anim anim voluptate dolore exercitation qui ut occaecat eiusmod fugiat."),
+        actions: [
+          TextButton(
+              onPressed: () {
+                context.pop();
+              },
+              child: const Text("Cancelar")),
+          FilledButton(
+              onPressed: () {
+                context.pop();
+              },
+              child: const Text("Aceptar"))
+        ],
+      ),
+    );
   }
 }
