4ae9bd58a256b9c6c38be6d25669f9f651942f50
Julio Adrian Ferreyra <jaft210403@outlook.com>
2023-06-27 11:59:26 -0600

    introduccion a metodos de providers por flutter_riverpod

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,19 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:widget_app/presentation/providers/counter_provider.dart';
+import 'package:widget_app/presentation/providers/theme_provider.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends ConsumerWidget {
   static const String name = "counter_screen";
 
   const CounterScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    int counter = 0;
+  Widget build(BuildContext context, WidgetRef ref) {
+    final bool isDarkMode = ref.watch(isDarkModeProvider);
+    final int counter = ref.watch(counterProvider);
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () {
+                ref.read(isDarkModeProvider.notifier).state = !isDarkMode;
+              },
+              icon: isDarkMode
+                  ? const Icon(Icons.light_mode)
+                  : const Icon(Icons.dark_mode))
+        ],
         title: const Text("Counter Screen"),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          ref.read(counterProvider.notifier).state++;
+          // ref.read(counterProvider.notifier).update((state) => state + 1);
+        },
         child: const Icon(Icons.plus_one),
       ),
       body: Center(
